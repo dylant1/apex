226657dcac09810750792d35e2853783b597c7af
Dylan Toth <dylant1@yahoo.com>
2022-06-28 15:21:35 -0500

    Setup basic logic to fetch apex stats

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = nextConfig;
+module.exports = {
+  env: {
+    apiKey: "64b194b9539b4f19a3295b64fa10c85a",
+  },
+};
